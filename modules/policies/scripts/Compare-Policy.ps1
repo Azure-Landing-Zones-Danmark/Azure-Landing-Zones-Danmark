24ed1d7e1057482268ca511b03907d9ce1450c5c
Rasmus Lystrøm <rasmus@lystroem.dk>
2022-10-05 12:07:58 +0200

    go

--- modules/policies/scripts/Compare-Policy.ps1
+++ modules/policies/scripts/Compare-Policy.ps1
@@ -10,16 +10,16 @@ param (
 )
 
 function Write-Compare ([Parameter(Mandatory = $true)][String]$Label, $Object, $SideIndicator, $Prefix) {
-    $deleted = $Object | Where-Object SideIndicator -eq $SideIndicator | ForEach-Object { "$Prefix $($PSItem.InputObject)" }
-    if ($deleted) {
+    $compare = $Object | Where-Object SideIndicator -eq $SideIndicator | ForEach-Object { "$Prefix $($PSItem.InputObject)" }
+    if ($compare) {
         Write-Output $Label
-        Write-Output $deleted
+        Write-Output $compare
     }
 }
 
 function Get-ResourceNameFromTemplate ($Path, $Pattern) {
     Get-ChildItem -Path $Path/*.bicep -Exclude ".deploy.bicep" | ForEach-Object {
-        $definition = $PSItem | Get-Content -Raw | Select-String -Pattern $Pattern
+        $definition = $PSItem | Get-Content | Select-String -Pattern $Pattern
         $definition.Matches[0].Groups[1].Value
     }
 }
@@ -27,12 +27,14 @@ function Get-ResourceNameFromTemplate ($Path, $Pattern) {
 function Compare-Item ($Item, $Label, $Path, $Pattern, $ManagementGroupId) {
     Write-Output "Comparing $($Label.ToLower()) under '$ManagementGroupId'..."
     $toBeDeployed = Get-ResourceNameFromTemplate -Path $Path -Pattern $Pattern
-    $deleted = Compare-Object -ReferenceObject ($toBeDeployed ?? @()) -DifferenceObject ($Item ?? @()) -IncludeEqual
+    $compare = Compare-Object -ReferenceObject ($toBeDeployed ?? @()) -DifferenceObject ($Item ?? @()) -IncludeEqual
     Write-Compare -Label "$Label to be created:" -Object $deleted -SideIndicator "<=" -Prefix "+"
     Write-Compare -Label "$Label to be updated:" -Object $deleted -SideIndicator "==" -Prefix "*"
     Write-Compare -Label "$Label to be deleted:" -Object $deleted -SideIndicator "=>" -Prefix "-"
 
-    $deleted | Where-Object SideIndicator -EQ "=>"
+    if ($compare | Where-Object SideIndicator -EQ "=>") {
+        $script:deleteDetected = $true
+    }
 }
 
 function Get-AssignmentGroup {
@@ -58,20 +60,16 @@ function Get-AssignmentGroup {
     }
 }
 
-$deleted = $false
-
 $definitions = Get-AzPolicyDefinition -ManagementGroupName $ManagementGroupRoot -Custom | Select-Object -ExpandProperty Name
-$deleted = $deleted -or (Compare-Item -Label "Definitions" -Item $definitions -Path "$Path/definitions" -ManagementGroupId $ManagementGroupRoot -Pattern "=\s+\{\s+name:\s+'(.+)\'")
+Compare-Item -Label "Definitions" -Item $definitions -Path "$Path/definitions" -ManagementGroupId $ManagementGroupRoot -Pattern "^name: '(.+)'$"
 
 $initiatives = Get-AzPolicySetDefinition -ManagementGroupName $ManagementGroupRoot -Custom | Select-Object -ExpandProperty Name
-$deleted = $deleted -or (Compare-Item -Label "Initiatives" -Item $initiatives -Path "$Path/initiatives" -ManagementGroupId $ManagementGroupRoot -Pattern "=\s+\{\s+name:\s+'(.+)\'")
+Compare-Item -Label "Initiatives" -Item $initiatives -Path "$Path/initiatives" -ManagementGroupId $ManagementGroupRoot -Pattern "^name: '(.+)'$"
 
 Get-AssignmentGroup -Path $Path -ManagementGroupRoot $ManagementGroupRoot | ForEach-Object {
     $path = $PSItem.Path
     $managementGroupId = $PSItem.ManagementGroupId
     $managementGroup = Get-AzManagementGroup -GroupName $managementGroupId
     $assignments = Get-AzPolicyAssignment -Scope $managementGroup.Id | Where-Object { $PSItem.Properties.Scope -eq $managementGroup.Id } | Select-Object -ExpandProperty Name
-    $deleted = $deleted -or (Compare-Item -Label "Assignments" -Item $assignments -Path $path -ManagementGroupId $managementGroupId -Pattern "policyAssignmentName:\s+'(.+)\'")
+    Compare-Item -Label "Assignments" -Item $assignments -Path $path -ManagementGroupId $managementGroupId -Pattern "^policyAssignmentName: '(.+)'$"
 }
-
-$deleted
