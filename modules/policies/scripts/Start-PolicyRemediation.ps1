6723c7999a0caed94c4696a41bcff464172d9fec
Rasmus Lystrøm <rasmus@lystroem.dk>
2022-10-06 08:51:21 +0200

    Remediate

--- modules/policies/scripts/Start-PolicyRemediation.ps1
+++ modules/policies/scripts/Start-PolicyRemediation.ps1
@@ -7,19 +7,24 @@ param (
 
 Get-AzPolicyState -ManagementGroupName $ManagementGroupRoot |
 Where-Object { $PSItem.ComplianceState -eq "NonCompliant" -and $PSItem.PolicyDefinitionAction -in "DeployIfNotExists", "Modify" } |
-Select-Object -Property PolicyAssignmentId, PolicyDefinitionReferenceId -Unique |
+Select-Object PolicyAssignmentId, PolicyDefinitionReferenceId -Unique |
 ForEach-Object {
     $policyAssignmentId = $PSItem.PolicyAssignmentId
     $policyDefinitionReferenceId = $PSItem.PolicyDefinitionReferenceId
     $remediation = $policyDefinitionReferenceId ? "$policyAssignmentId/$policyDefinitionReferenceId" : $policyAssignmentId
 
-    $running = Get-AzPolicyRemediation -ManagementGroupName $ManagementGroupRoot -Filter "ProvisioningState eq 'Running' && PolicyAssignmentId eq '$policyAssignmentId' && PolicyDefinitionReferenceId eq '$policyDefinitionReferenceId'"
+    $running = Get-AzPolicyRemediation -ManagementGroupName $ManagementGroupRoot -Filter "PolicyAssignmentId eq '$policyAssignmentId' and PolicyDefinitionReferenceId eq '$policyDefinitionReferenceId'" | Where-Object ProvisioningState -ne "Running"
 
     if ($running) {
         Write-Warning "Remediation for $remediation is already running"
     }
     else {
         Write-Output "Remediating $remediation under $ManagementGroupRoot"
-        Start-AzPolicyRemediation -ManagementGroupName $ManagementGroupRoot -PolicyAssignmentId $policyAssignmentId -PolicyDefinitionReferenceId $policyDefinitionReferenceId
+        if ($policyDefinitionReferenceId) {
+            Start-AzPolicyRemediation -Name (New-Guid) -ManagementGroupName $ManagementGroupRoot -PolicyAssignmentId $policyAssignmentId -PolicyDefinitionReferenceId $policyDefinitionReferenceId
+        }
+        else {
+            Start-AzPolicyRemediation -Name (New-Guid) -ManagementGroupName $ManagementGroupRoot -PolicyAssignmentId $policyAssignmentId
+        }
     }
 }
