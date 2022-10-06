5fdce40606660e25afc24e94b45e8a81dd4234ce
Rasmus Lystrøm <rasmus@lystroem.dk>
2022-10-06 11:18:34 +0200

    up

--- scripts/Remove-ManagementGroup.ps1
+++ scripts/Remove-ManagementGroup.ps1
@@ -4,7 +4,7 @@ Remove all management groups in hierarchy up to and including prefix
 #>
 [CmdletBinding(SupportsShouldProcess)]
 param (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, Position = 0)]
     [String]
     $Prefix
 )
@@ -12,29 +12,17 @@ param (
 $tenantId = (Get-AzContext).Tenant.Id
 
 Get-AzManagementGroup | Where-Object Name -match "^$Prefix" | Sort-Object -Descending | ForEach-Object {
-    $scope = $PSItem.Id
-    $name = $PSItem.Name
-    $displayName = $PSItem.DisplayName
+    $managementGroupName = $PSItem.Name
+    $managementGroupDisplayName = $PSItem.DisplayName
 
-    Write-Verbose "Removing policy assignments from management group '$displayName'..."
+    Write-Output "Checking subscriptions under management group '$managementGroupDisplayName'..."
 
-    Get-AzPolicyAssignment -Scope $scope | Where-Object { $PSItem.Properties.Scope -eq $scope } | Remove-AzPolicyAssignment
-
-    Write-Verbose "Removing initiatives from management group '$displayName'..."
-
-    Get-AzPolicySetDefinition -ManagementGroupName $name | Remove-AzPolicySetDefinition
-
-    Write-Verbose "Removing policy definitions from management group '$displayName'..."
-
-    Get-AzPolicyDefinition -ManagementGroupName $name | Remove-AzPolicyDefinition
-
-    Get-AzManagementGroupSubscription -GroupName $name | ForEach-Object {
+    Get-AzManagementGroupSubscription -GroupName $managementGroupName | ForEach-Object {
         $subscriptionId = $PSItem.Id -split "/" | Select-Object -Last 1
-        Write-Verbose "Moving subscription $($PSItem.DisplayName) to /"
-        New-AzManagementGroupSubscription -GroupName $tenantId -SubscriptionId $subscriptionId
+        Write-Output "Moving subscription '$managementGroupDisplayName' to /..."
+        New-AzManagementGroupSubscription -GroupName $tenantId -SubscriptionId $subscriptionId | Out-Null
     }
 
-    Write-Verbose "Removing management group '$displayName'..."
-
-    Remove-AzManagementGroup -GroupName $name
+    Write-Output "Removing management group '$managementGroupDisplayName'..."
+    Remove-AzManagementGroup -GroupName $managementGroupName | Out-Null
 }
