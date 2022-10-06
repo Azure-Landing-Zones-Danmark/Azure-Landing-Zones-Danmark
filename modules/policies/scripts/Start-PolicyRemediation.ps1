8dad454478c3d40dccc3f759bbafb9f4e014ad87
Rasmus Lystrøm <rasmus@lystroem.dk>
2022-10-06 11:47:19 +0200

    Fix remediation (#10)

--- modules/policies/scripts/Start-PolicyRemediation.ps1
+++ modules/policies/scripts/Start-PolicyRemediation.ps1
@@ -2,24 +2,29 @@
 param (
     [Parameter(Mandatory = $true)]
     [String]
-    $ManagementGroupRoot
+    $Prefix
 )
 
-Get-AzPolicyState -ManagementGroupName $ManagementGroupRoot |
+Get-AzPolicyState -ManagementGroupName $Prefix |
 Where-Object { $PSItem.ComplianceState -eq "NonCompliant" -and $PSItem.PolicyDefinitionAction -in "DeployIfNotExists", "Modify" } |
-Select-Object -Property PolicyAssignmentId, PolicyDefinitionReferenceId -Unique |
+Select-Object PolicyAssignmentId, PolicyDefinitionReferenceId -Unique |
 ForEach-Object {
     $policyAssignmentId = $PSItem.PolicyAssignmentId
     $policyDefinitionReferenceId = $PSItem.PolicyDefinitionReferenceId
     $remediation = $policyDefinitionReferenceId ? "$policyAssignmentId/$policyDefinitionReferenceId" : $policyAssignmentId
 
-    $running = Get-AzPolicyRemediation -ManagementGroupName $ManagementGroupRoot -Filter "ProvisioningState eq 'Running' && PolicyAssignmentId eq '$policyAssignmentId' && PolicyDefinitionReferenceId eq '$policyDefinitionReferenceId'"
+    $running = Get-AzPolicyRemediation -ManagementGroupName $Prefix -Filter "PolicyAssignmentId eq '$policyAssignmentId' and PolicyDefinitionReferenceId eq '$policyDefinitionReferenceId'" | Where-Object ProvisioningState -ne "Running"
 
     if ($running) {
         Write-Warning "Remediation for $remediation is already running"
     }
     else {
-        Write-Output "Remediating $remediation under $ManagementGroupRoot"
-        Start-AzPolicyRemediation -ManagementGroupName $ManagementGroupRoot -PolicyAssignmentId $policyAssignmentId -PolicyDefinitionReferenceId $policyDefinitionReferenceId
+        Write-Output "Remediating $remediation under $Prefix"
+        if ($policyDefinitionReferenceId) {
+            Start-AzPolicyRemediation -Name (New-Guid) -ManagementGroupName $Prefix -PolicyAssignmentId $policyAssignmentId -PolicyDefinitionReferenceId $policyDefinitionReferenceId
+        }
+        else {
+            Start-AzPolicyRemediation -Name (New-Guid) -ManagementGroupName $Prefix -PolicyAssignmentId $policyAssignmentId
+        }
     }
 }
