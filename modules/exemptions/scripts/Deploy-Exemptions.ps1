332dafdb21e5753ea6cd679528cdb8c0806baa70
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2022-11-29 06:32:19 +0100

    exemptions

--- modules/exemptions/scripts/Deploy-Exemptions.ps1
+++ modules/exemptions/scripts/Deploy-Exemptions.ps1
@@ -3,7 +3,9 @@ param (
     [String]$Path = '.\modules\Exemptions',
     [String]$Location = 'WestEurope',
     [String]$templateFile  = '.\modules\shared\policyExemptions.bicep',
-    [Bool]$removeUnmanagedExemptions = $False
+    [Parameter(Mandatory,)]
+     [ValidateSet('true','false')]
+    [String]$removeUnmanagedExemptions = "false"
 )
 
 # Get existing Exemptions
@@ -35,7 +37,7 @@ foreach($exemption in $exemptions){
 }
 
 #Remove Un-manged Exemptions
-If($removeUnmanagedExemptions -eq $true){
+If($removeUnmanagedExemptions -eq 'true'{
     $deployedExemptions = Get-AzPolicyExemption | Select-Object -ExpandProperty Name
     foreach($deployedExemption in $deployedExemptions){
         if(-not($deployedExemption -match $exemptionNames)){
